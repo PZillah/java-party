7b6b00eab0853502c2c5cd32b30cb00afd883f39
priscilla <priscillakung@gmail.com>
2023-10-03 14:39:03 -0700

    create a rectangle class

---
+++ Rectangle.java
@@ -0,0 +1,41 @@
+import java.awt.Color;
+
+public class Rectangle {
+    // FIELDS
+    private int x, y, width, height;
+    private Color color;
+
+    // CONSTRUCTORS
+    public Rectangle() {
+        x = 100;
+        y = 100;
+        width = 100;
+        height = 200;
+        color = Color.CYAN;
+    }
+
+    public Rectangle(int x, int y, int width, int height, Color color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    // METHODS
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        System.out.println("Width is: " + r.getWidth());
+        r.setWidth(1000);
+        System.out.println("New width is: " + r.getWidth());
+    }
+
+}
